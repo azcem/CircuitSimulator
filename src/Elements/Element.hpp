a596ca035a863a03b6e6bba8773ddef156a6d41e
Assem Ahmed <assem.a.r.askar@gmail.com>
2025-03-22 22:48:20 +0200

    modify Element class to use nodes dict instead of inheritance

--- src/Elements/Element.hpp
+++ src/Elements/Element.hpp
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 
 /* Enum for optional group clause */
 enum class Group {
-	NO_GROUP,
+	G1,
 	G2
 };
 
@@ -18,7 +19,16 @@ class Element {
 public:
 	Element(std::string& name): m_name{name} {};
 	void setName(std::string& name) { m_name = name; }
-	std::string getName() { return m_name; }
+	std::string getName() const { return m_name; }
+	void setValue(double value) { m_value = value; }
+	double getValue() const { return m_value; }
+	auto& operator[](const std::string& key) { return m_nodes[key]; }
+	Group getGroup() const { return m_group; }
+	void setGroup(Group group) { m_group = group; }
+
 private:
 	std::string m_name;
+	double m_value;
+	std::unordered_map<std::string, int> m_nodes;
+	Group m_group = Group::G1;
 };
